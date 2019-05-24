82395b1927bd59505537149902c15d22963275a5
Josue Leyva <Josue@eduroam-169-233-160-227.ucsc.edu>
2019-05-23 17:20:37 -0700

    Update main1.java
    updated name

--- main1.java
+++ main1.java
@@ -2,9 +2,14 @@ public class  main1{
   public static void main(Strings[] args){
     //comment
     //added
-    String hell = "Hell"; 
+    String hell = "Hell";
     int x = 70;
     double twenty = 20;
     //this is it
+    //another one
+    int three = 3 ;
+   }
+   public static void name(){
+     
    }
 }
